b9c66f164f0781c4090a4068b0e766243206a67c
Rune <sungjong5k@naver.com>
2025-11-22 22:56:19 +0900

    feat: add sea level animation

--- src/constants/climateMap.tsx
+++ src/constants/climateMap.tsx
@@ -2,8 +2,9 @@ import RainAnimation from "../components/animation/RainAnimation";
 import DroughtAnimation from "../components/animation/DroughtAnimation";
 import FineDustAnimation from "../components/animation/FineDustAnimation";
 import EarthquakeAnimation from "../components/animation/EarthquakeAnimation";
-import { Climate } from "../types/climate";
 import WildfireAnimation from "../components/animation/WildfireAnimation";
+import { Climate } from "../types/climate";
+import SeaLevelRiseAnimation from "../components/animation/SeaLevelRiseAnimation";
 
 export const climateMap: Partial<Record<Climate, React.ReactNode>> = {
   HEAVY_RAIN_OR_FLOOD: <RainAnimation dropNum={400} />,
@@ -11,4 +12,5 @@ export const climateMap: Partial<Record<Climate, React.ReactNode>> = {
   FINE_DUST: <FineDustAnimation />,
   EARTHQUAKE: <EarthquakeAnimation />,
   WILDFIRE: <WildfireAnimation />,
+  SEA_LEVEL_RISE: <SeaLevelRiseAnimation />,
 };
